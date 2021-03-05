35b8e86ca4ad3462e0bbc1071c0b360046191db5
danoman17 <daniflores374@gmail.com>
2021-03-05 12:32:49 -0600

    Add Navbar and Initial commmit - DFR

---
+++ views/index.view.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://kit.fontawesome.com/fdd2e8457c.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/mainStyles.css">
+    <title>Websmiths</title>
+</head>
+<body>
+    
+     <!-- Parte inicial de contacto -->
+
+     <div class="contacto_rapido">
+        <p>Tel. 55 45 32 45 79  |  email: websmiths123@gmail.com</p>
+        <a href="#"> <div class="whats-nav"><i class="fab fa-whatsapp"></i></div></a>
+        <a href="#"> <div class="face-nav"><i class="fab fa-facebook"></i></div> </a>
+        <a href="#"> <div class="insta-nav"><i class="fab fa-instagram"></i></div> </a>
+    </div>
+
+    <!-- responsive menu -->
+    <div class="menu-resposive" id="menu-responsive">
+            <ul>
+                <li><a href="#" id="btn-inicio-alterado">Inicio</a></li>
+                <li><a href="#" id="btn-sobre-mi-alterado">Sobre mi</a></li>
+                <li><a href="#" id="btn-servicios-alterado">Servicios</a></li>
+                <li><a href="#" id="btn-horarios-alterado">Horarios</a></li>
+                <li><a href="#" id="btn-ubicacion-alterado">Ubicación</a></li>
+                <li><a href="#" id="btn-contacto-alterado">Contacto</a></li>
+            </ul>
+    </div>
+    <!-- header -->
+    <nav id="nav">
+    <input type="checkbox" id="check" >
+			<label for="check" class="check_btn">
+				<i class="fas fa-bars"></i>
+			</label>
+        <div class="izq">
+            <div class="logo">
+                <div class="img-logo">
+                </div>
+                <!-- <div class="nombre-logo">
+                    <p>WEBSMITHS</p>
+                </div> -->
+            </div>
+        </div>
+        <div class="der">
+            <ul>
+                <li><a href="#" id="btn-inicio">Inicio</a></li>
+                <li><a href="#" id="btn-sobre-mi">Nosotros</a></li>
+                <li><a href="#" id="btn-servicios">Servicios</a></li>
+                <li><a href="#" id="btn-ubicacion">¿Website?</a></li>
+                <li><a href="#" id="btn-horarios">Contacto</a></li>
+                
+            </ul>
+        </div>
+        
+    </nav>
+
+
+    <!-- Inicio -->
+
+    <section class="home">
+        
+    </section>
+
+
+</body>
+</html>
